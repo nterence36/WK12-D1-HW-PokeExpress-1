eb7a39de2fcaa5f8f87092c621453f96425bda08
Terence Nkeng <nterence36@gmail.com>
2023-09-19 18:46:35 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const jsxEngine = require("jsx-view-engine");
 const pokemon = require("./models/pokemon.js");
+
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine()); 
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 
---
+++ views/Index.jsx
@@ -0,0 +1,20 @@
+const React = require("react");
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+    };
+
+
+    class Index extends React.Component {
+        render() {
+        
+          return (
+            <div style={myStyle}>
+              <h1>Fruits Index Page</h1>
+              
+            </div>
+          );
+        }
+      }
+      module.exports = Index;
+      
