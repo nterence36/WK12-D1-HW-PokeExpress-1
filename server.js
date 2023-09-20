bfe206d9d0bcfc064d2d20019c65c393433edf58
Terence Nkeng <nterence36@gmail.com>
2023-09-19 21:36:46 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/pokemon', (req, res) => {
     res.render('Index', {pokemon:pokemon})
 })
 
+//Show route
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
 app.listen(port,() => {
     console.log('listening on port' , port);
